31bbd33e124750f26b36b1d5560cc8dae3ee2394
Leif Haberecht <deskilling@tuta.com>
2026-07-23 22:58:58 +0200

    update version

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
       {
         packages.awallpicker = pkgs.stdenv.mkDerivation {
           pname = "awallpicker";
-          version = "0.4.0";
+          version = "0.4.1";
           src = ./.;
 
           nativeBuildInputs = with pkgs; [
